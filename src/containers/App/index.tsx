d1ff987d11669803a7c3e2036fd243709875dfd4
alexalemanhdz <alex.aleman.public@gmail.com>
2021-06-30 23:47:14 -0500

    Added ESLint

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,12 +1,12 @@
-import * as React from "react";
-import { hot } from "react-hot-loader";
+import * as React from 'react';
+import { hot } from 'react-hot-loader';
 
 const App = () => {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
-}
+	return (
+		<div>
+			<h1>Hello, World!</h1>
+		</div>
+	);
+};
 
 export default hot(module)(App);

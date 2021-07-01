10a3b0a942ec50c90a89059b45e4ae346c7f57f9
alexalemanhdz <alex.aleman.public@gmail.com>
2021-06-30 21:09:25 -0500

    Added TS... kinda

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   mode: "development",
   module: {
     rules: [
